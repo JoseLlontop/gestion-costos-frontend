b21158ce7ffa779b48cb58347bb07930c11c6268
JoseLlontop <josellontop100@gmail.com>
2024-09-17 00:38:16 -0300

    Se agrego la estructura de la pantalla de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
