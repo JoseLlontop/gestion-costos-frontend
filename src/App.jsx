abcc55042036a0a2bf62dc2778cc8f12b7b96579
JoseLlontop <josellontop100@gmail.com>
2024-09-28 16:55:07 -0300

    Se definio la ruta de la nevegacion de la aplicacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Header, Footer } from "./components";
+import { Home, GestionIngredientes } from "./pages";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Main />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='gestionIngredientes' element={<GestionIngredientes/>}/>
+        </Routes>
+
+        <Footer />    
+      </BrowserRouter>
     </div>
   );
 }
