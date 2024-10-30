a4ecc212bf8d81936f035f266f052d3dae2cb097
ivan sarria <ivansarria@alu.frlp.utn.edu.ar>
2024-10-30 08:17:38 -0300

    dialogo de confirmacion de update porcentaje ganancia

--- src/pages/costos/AplicarCambiosButton.jsx
+++ src/pages/costos/AplicarCambiosButton.jsx
@@ -1,7 +1,17 @@
-import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from "@mui/material";
 
 function AplicarCambiosButton({ recetaId, porcentajeGanancia }) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     const handleClick = async () => {
         try {
             const response = await fetch(
@@ -21,15 +31,33 @@ function AplicarCambiosButton({ recetaId, porcentajeGanancia }) {
 
             const data = await response.json();
             console.log("Item agregado : ", data);
+
+            handleOpen();
         } catch (error) {
             console.log("Error al actualizar porcentaje de ganancia:", error);
         }
     };
 
     return (
-        <Button variant="contained" color="success" onClick={handleClick}>
-            Aplicar Cambios
-        </Button>
+        <>
+            <Button variant="contained" color="success" onClick={handleClick}>
+                Aplicar Cambios
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Cambio Confirmado</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Se actualizo el porcentaje de ganancia a{" "}
+                        <strong>{porcentajeGanancia}%</strong>.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="error">
+                        Cerrar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 }
 
